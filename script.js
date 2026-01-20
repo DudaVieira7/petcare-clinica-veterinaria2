3b223b72815065a66f80d55c82e964456718f5dd
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-01-20 17:55:46 -0300

    atualizacao na paleta de cores e identidade visual

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ todasPerguntas.forEach((faq) =>{
             resposta.style.display = "block";
             up.style.display = "block";
             down.style.display = "none";
-            pergunta.style.background = "#572e99";
+            pergunta.style.background = "#3b1f67";
             pergunta.style.color = "#fff";
 
 
